2b365374cc779b55c1a3f05c4a08419db98ccc71
Manmohan Tripathi <tripathimanmohan170@gmail.com>
2026-06-26 10:48:21 +0530

    learn to say hello

---
+++ Switch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (1-4) to learn hello in different languages: ");
+        int number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("You selected option 1.(Hello in HIndi!) : "+"Namaste");
+                break;
+            case 2:
+                System.out.println("You selected option 2.(Hello in Spanish!) : "+"Hola");
+                break;
+            case 3:
+                System.out.println("You selected option 3.(Hello in French!) : "+"Bonjour");
+                break;
+            case 4:
+                System.out.println("You selected option 4.(Hello in Japanese!) : "+"Konichiwa");
+                break;
+            default:
+                System.out.println("Invalid option. Please select a number between 1 and 4.");
+        }
+
+        scanner.close();
+    }
+}       
