ffc4e786a0499c2f0047f301196070a01368ab19
Manmohan Tripathi <tripathimanmohan170@gmail.com>
2026-07-17 20:26:53 +0530

    Add option for German greeting in Switch program

--- Switch.java
+++ Switch.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number (1-4) to learn hello in different languages: ");
+        System.out.print("Enter a number (1-5) to learn hello in different languages: ");
         int number = scanner.nextInt();
 
         switch (number) {
@@ -18,8 +18,11 @@ public class Switch {
             case 4:
                 System.out.println("You selected option 4.(Hello in Japanese!) : "+"Konichiwa");
                 break;
+                case 5:
+                System.out.println("You selected option 5.(Hello in German!) : "+"Guten Tag");
+                break;
             default:
-                System.out.println("Invalid option. Please select a number between 1 and 4.");
+                System.out.println("Invalid option. Please select a number between 1 and 5.");
         }
 
         scanner.close();
